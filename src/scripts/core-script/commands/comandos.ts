9d46d7beb3fd93e23f433017b6568a0cd0ada598
Agustin F.N <agusfn20@gmail.com>
2024-03-10 23:31:29 +0100

    Comandos list (wip)

--- src/scripts/core-script/commands/comandos.ts
+++ src/scripts/core-script/commands/comandos.ts
@@ -1,5 +1,5 @@
 import { DiscordCommand } from "@ts/interfaces";
-import { SlashCommandBuilder } from "discord.js";
+import { APIApplicationCommandOption, ApplicationCommandOptionType, PermissionsBitField, SlashCommandBuilder } from "discord.js";
 import { CoreScript } from "../CoreScript";
 
 
@@ -13,13 +13,76 @@ export default {
 		
 		let text = ""
 		for(const groupName of Object.keys(groupedCommands)) {
+
 			text += "\n**__" + groupName + "__**\n"
-			for(const command of groupedCommands[groupName]) {
-				text += "**/" + command.data.name + "**" + (command.data.description ? ": " + command.data.description : " (sin descripción)") + "\n"
+
+			for(const { data } of groupedCommands[groupName]) {
+
+
+				console.log("command", data.name);
+				console.log("perm", data.default_member_permissions);
+				const perm = interaction.member.permissions as PermissionsBitField;
+				if(perm.has(data.default_member_permissions as any)) {
+					console.log("HAS PERM")
+				} else {
+					console.log("NO PERM")
+				}
+
+
+
+				//console.log("options", JSON.stringify(data.options, null, 4));
+
+				const subcommandList = getSubcommandList(data);
+
+				text += "**/" + data.name + "**";
+				if(subcommandList) {
+					text += " _`subcomando`_";
+				} else {
+					text += getCommandParamList(data);
+				}
+				text += (data.description ? ": " + data.description : " (sin descripción)") + "\n";
+
+				if(subcommandList) {
+					text += subcommandList + "\n";
+				}
 			}
 		}
 
-		await interaction.reply(text);
-
+		await interaction.reply({ content: text, ephemeral: true });
 	},
 } as DiscordCommand<CoreScript>;
+
+
+
+const getSubcommandList = (command: SlashCommandBuilder) => {
+
+	let list = "";
+	for(const option of command.options.map(op => op.toJSON())) {
+
+		if(option.type == ApplicationCommandOptionType.Subcommand) {
+			list += `_   _\- **${option.name}**${getCommandParamList(option as any)}: ` + option.description + "\n";
+		}
+	}
+
+	return list || null;
+}
+
+
+const getCommandParamList = (command: SlashCommandBuilder): string => {
+	
+	let list = "";
+
+	if(command.options.length > 0) {
+
+		const options: any[] = command.options[0].toJSON ? command.options.map(op => op.toJSON()) : command.options;
+
+		for(const option of options) {
+			if(option.type != ApplicationCommandOptionType.Subcommand && option.type != ApplicationCommandOptionType.SubcommandGroup) {
+				list += " _`"+option.name+"`_";
+			}
+		}
+
+	}
+
+	return list;
+}
